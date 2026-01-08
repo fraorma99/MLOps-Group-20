7549583742cade742d434f4922165a8022768fcf
Kenzo Kenzo <161597014+KenzoDTU@users.noreply.github.com>
2026-01-09 00:02:53 +0100

    ruff?

--- src/mlops_group_20/train.py
+++ src/mlops_group_20/train.py
@@ -1,10 +1,12 @@
 from mlops_group_20.model import Model
 from mlops_group_20.data import MyDataset
 
+
 def train():
     dataset = MyDataset("data/raw")
     model = Model()
     # add rest of your training code here
+
 
 if __name__ == "__main__":
     train()
