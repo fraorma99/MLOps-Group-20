db3f6efeeef9f035383353fe1c7d73841da6daa4
GeorgiosPapageorgiou99 <s253248@dtu.dk>
2026-01-16 12:36:18 +0100

    Updated code for test data

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+TEST_ROOT = os.path.dirname(file_)  # root of test folder
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
