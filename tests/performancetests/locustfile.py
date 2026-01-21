a4fdfaa251c6eb12a74c68e420b69e77978fc50d
raulalcazarr <raulalcazarrr@gmail.com>
2026-01-21 13:17:09 +0100

    latests run documets

--- tests/performancetests/locustfile.py
+++ tests/performancetests/locustfile.py
@@ -2,12 +2,12 @@ from locust import HttpUser, task
 
 class MyUser(HttpUser):
     """My user class that defines the tasks to be performed by the user."""
-    
+
     @task
     def index(self):
         """GET / - user visiting the root URL of the FastAPI app"""
         self.client.get("/")
-        
+
     @task(1)
     def user(self):
         """GET /user/16 - user visiting a specific user page"""
